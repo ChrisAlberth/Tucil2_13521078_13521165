4c2563e73115f6f31c3343cd3310f3f377e702b2
Chris Albert <christiangultom5@gmail.com>
2023-03-01 01:50:36 +0700

    style: main menu and add platform spec

--- src/main.py
+++ src/main.py
@@ -1,33 +1,55 @@
 import point
 import time
 import plot
+import platform
 
 if __name__ =="__main__":
     print("Selamat datang di program Find Closest Point!!!")
+    
+    while (True):
+        print("\nMenu")
+        print("1. 3 Dimensional Space")
+        print("2. Euclidean Space (Vector Rn)")
+        print("3. Keluar Program")
+        
+        menuInp = int(input("Masukan pilihan menu: "))
+        
+        if (menuInp not in [1,2,3]):
+            print("Pilihan menu tidak ada. Pilih kembali menu!")
+        else:
+            if (menuInp == 3):
+                print("Terima kasih sudah menggunakan program kami. Sampai jumpa")
+                exit()
+            else:
+                if (menuInp == 1):
+                    nDim = 3
+                else:
+                    nDim = int(input("Dimensi: "))
+                    
+                nPoint = int(input("Banyak point: "))
+                p = point.initializePoint(nPoint, nDim)
 
-    nPoint = int(input("Banyak point: "))
-    nDim = int(input("Dimensi: "))
-    p = point.initializePoint(nPoint, nDim)
-    
-    print("Closest pair dengan Brute Force:")
-    t1 = time.time()
-    distBF, bf1, bf2, c1 = point.closestPairBF(p)
-    t2 = time.time()
-    print("Jarak terdekat", distBF)
-    print("Titik 1:", bf1)
-    print("Titik 2:", bf2)
-    print("Waktu eksekusi (s):", t2-t1)
-    print("Banyak operasi euclidean distance:", c1)
-    
-    print("Closest pair dengan Divide and Conquer:")
-    t1 = time.time()
-    distDC, dc1, dc2, c2 = point.closestPairDNC(p)
-    t2 = time.time()
-    print("Jarak terdekat", distDC)
-    print("Titik 1:", dc1)
-    print("Titik 2:", dc2)
-    print("Waktu eksekusi (s):", t2-t1)
-    print("Banyak operasi euclidean distance:", c2)
-    
-    if nDim==3:
-        plot.plot_3d(p, bf1, bf2)
+                print("Closest pair dengan Brute Force:")
+                t1 = time.time()
+                distBF, bf1, bf2, c1 = point.closestPairBF(p)
+                t2 = time.time()
+                print("Jarak terdekat", distBF)
+                print("Titik 1:", bf1)
+                print("Titik 2:", bf2)
+                print("Waktu eksekusi (s):", t2-t1)
+                print("Banyak operasi Euclidean Distance:", c1)
+                print("Running on", platform.platform(), platform.processor())
+
+                print("\nClosest pair dengan Divide and Conquer:")
+                t1 = time.time()
+                distDC, dc1, dc2, c2 = point.closestPairDNC(p)
+                t2 = time.time()
+                print("Jarak terdekat", distDC)
+                print("Titik 1:", dc1)
+                print("Titik 2:", dc2)
+                print("Waktu eksekusi (s):", t2-t1)
+                print("Banyak operasi Euclidean Distance:", c2)
+                print("Running on", platform.platform(), platform.processor())
+
+                if nDim==3:
+                    plot.plot_3d(p, bf1, bf2)
